6956c66f1b8a0ed16d20f25a557ec80383628c69
pK <p.kruchinin@globus-it.com>
2022-06-16 20:48:52 +0200

    create empty project

---
+++ WorkoutApp/WorkoutApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Pavel Olegovich on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+}
+
