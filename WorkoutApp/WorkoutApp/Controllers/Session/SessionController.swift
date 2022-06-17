681d704fdcf2cf086591d20fe97b4d069b95b916
pK <p.kruchinin@globus-it.com>
2022-06-18 01:41:53 +0200

    update configuration for baseInfoView

--- WorkoutApp/WorkoutApp/Controllers/Session/SessionController.swift
+++ WorkoutApp/WorkoutApp/Controllers/Session/SessionController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class SessionController: BaseController {
+    private let timerView: BaseInfoView = {
+        let view = BaseInfoView(with: "Test", buttonTitle: "Test button")
+        return view
+    }()
+    
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+extension SessionController {
+    override func setupViews() {
+        super.setupViews()
+        
+        view.addView(timerView)
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
+        
+        NSLayoutConstraint.activate([
+            timerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            timerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            timerView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
         title = R.Strings.NavBar.session
         navigationController?.tabBarItem.title = R.Strings.TabBar.title(for: .session)
 
@@ -19,4 +42,3 @@ class SessionController: BaseController {
         addNavigationBarButton(at: .right, with: R.Strings.Session.navBarRight)
     }
 }
-
